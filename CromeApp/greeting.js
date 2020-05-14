5eabf1e39a62aa706d88122b6e0319d3c64e5aa9
blair06 <hyewon980610@gmail.com>
2020-05-14 22:25:56 +0900

    save name

--- CromeApp/greeting.js
+++ CromeApp/greeting.js
@@ -1,9 +1,12 @@
 const form = document.querySelector(".js-form")
 const input = form.querySelector("input");
-const USER_LS ="currentUser",
-SHOWING_CN = "showing";
-const greeting = document.querySelector(".js-greeting");
+const USER_LS ="currentUser"
+const SHOWING_CN = "showing";
+const greeting = document.querySelector(".js-greetings");
 
+function askForName(){
+    form.classList.add(SHOWING_CN);
+}
 function paintGreeting(text){
     form.classList.remove(SHOWING_CN);
     greeting.classList.add(SHOWING_CN);
@@ -12,12 +15,12 @@ function paintGreeting(text){
 function loadName(){
     const currentUser = localStorage.getItem(USER_LS);
     if(currentUser === null){
-
+        askForName();
     } else{
         paintGreeting(currentUser);
     }
 }
 function init(){
-
+    loadName();
 }
 init();
